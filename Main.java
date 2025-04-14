741545d986a4d2f01592b5f381a89297ff47494f
Kawczini300674 <i.kawczynski.084@studms.ug.edu.pl>
2025-04-14 15:34:07 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,11 +2,10 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("1. Piotr" +
-                " 2. Anna" +
-                " 3. Jan" +
-                " 4. Leszek");
-
-
+        System.out.println("1. Piotr");
+        System.out.println("2. Anna");
+        System.out.println("3. Jan");
+        System.out.println("4. Leszek");
+        
         }
     }
